3ab9bebf9161e368035b0d41bcaf656378e1be36
Suraj jadhav <114412313+surajjadhav1@users.noreply.github.com>
2022-10-27 19:59:03 +0530

    Update Program.java

--- Assign5/Program.java
+++ Assign5/Program.java
@@ -4,10 +4,10 @@ public class Program {
 
 	public static void main(String[] args){
 
-	int x = Integer.parseInt(args[0]);	
-	int y = Integer.parseInt(args[1]);
-	int z = Integer.parseInt(args[2]);
-	int temp = Integer.parseInt(args[3]);
+	//int x = Integer.parseInt(args[0]);	
+	//int y = Integer.parseInt(args[1]);
+	//int z = Integer.parseInt(args[2]);
+	//int temp = Integer.parseInt(args[3]);
 
 	Scanner s=new Scanner(System.in);
 
